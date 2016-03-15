817441572ae68e293dc85c6af35d0632329e76c7
Mirko Dietrich <mirko.dietrich@l4m1.de>
2016-03-15 18:03:52 +0100

    remove deleted events (closes #1)

--- ics2owncloud.py
+++ ics2owncloud.py
@@ -10,14 +10,26 @@ import requests
 from ics import Calendar
 
 
-CALDAVURL = '%sremote.php/dav/calendars/%s/%s/%s.ics'
+CALDAVURL = '%sremote.php/dav/calendars/%s/%s'
 
 def do_import(username, password, calendar, server, ics_url):
+  base_url = CALDAVURL % (server, username, calendar)
+
+  # fetch events from target cal
+  target_fetch_url = '%s?export' % base_url
+  target_cal = Calendar(
+    requests.get(target_fetch_url, auth=(username, password)).text)
+  existing_uids = [e.uid for e in target_cal.events]
+
+  # fetch webcal
   c = Calendar(requests.get(ics_url).text)
+
+  # import webcal
+  imported_uids = []
   for e in c.events:
     caldata = str(Calendar(events=[e]))
-    url = CALDAVURL % (server, username, calendar, e.uid)
-    r = requests.put(url,
+    target_url = '%s/%s.ics' % (base_url, e.uid)
+    r = requests.put(target_url,
                      data=caldata,
                      auth=(username, password),
                      headers={'content-type':'text/calendar; charset=UTF-8'}
@@ -30,8 +42,24 @@ def do_import(username, password, calendar, server, ics_url):
         r.raise_for_status()
     elif r.status_code == 201 or r.status_code == 204:
       print('Imported: %s (%d)' % (e.uid, r.status_code), file=sys.stdout)
+      imported_uids.append(e.uid)
     else:
-      print('Import failed: %s (%d)' % (e.uid, r.status_code), file=sys.stderr)
+      raise Exception('Import failed: %s (%d)' % (e.uid, r.status_code))
+
+  # remove events not in webcal
+  for uid in existing_uids:
+    if not uid in imported_uids:
+      target_url = '%s/%s.ics' % (base_url, uid)
+      r = requests.delete(target_url,
+                          data=caldata,
+                          auth=(username, password)
+                          )
+      r.raise_for_status()
+      if r.status_code == 204:
+        print('Deleted %s' % uid, file=sys.stdout)
+      else:
+        raise Exception('Unexpected return code while deleting: %s (%d)'
+                        % (e.uid, r.status_code))
 
 if __name__ == '__main__':
   Config = ConfigParser.ConfigParser()
